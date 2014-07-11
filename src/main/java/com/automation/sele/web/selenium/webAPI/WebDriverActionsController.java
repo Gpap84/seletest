59eee7bd5c276ad6a4ce4d2e6d7ff8692baaffff
GiannisPapadakis <gpapadakis84@gmail.com>
2014-07-11 21:50:27 +0300

    WebActionsController and WebDriver implementation

---
+++ src/main/java/com/automation/sele/web/selenium/webAPI/WebDriverActionsController.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.automation.sele.web.selenium.webAPI;
+
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.automation.sele.web.services.SynchronizeService;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * This class contains the implementation of webDriver API 
+ * for interaction with UI
+ * @author Giannis Papadakis(mailTo:gpapadakis84@gmail.com)
+ *
+ */
+@SuppressWarnings("unchecked")
+public class WebDriverActionsController implements WebActionsController{
+
+	@Autowired
+	SynchronizeService waitFor;
+	
+	/**The webDriver object*/
+	@Getter @Setter WebDriver driver;
+	
+	@Override
+	public WebDriverActionsController clickTo(Object locator) {
+		waitFor.waitForElementPresence(driver, 1, (String)locator).click();
+		return this;
+	}
+
+	@Override
+	public WebDriverActionsController enterTo(Object locator, String text) {
+		waitFor.waitForElementPresence(driver, 1, (String)locator).sendKeys(text);
+		return this;
+	}
+
+}
