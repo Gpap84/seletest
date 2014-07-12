835f706f624a1aed935786446f1b4554e144ca80
GiannisPapadakis <gpapadakis84@gmail.com>
2014-07-12 14:46:41 +0300

    Custom annotations for post and pre configure of a test method or test class

---
+++ src/main/java/com/automation/sele/web/testNG/PostConfigure.java
@@ -0,0 +1,27 @@
+package com.automation.sele.web.testNG;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation executes method of a class after invocation of a method
+ * @author Giannis Papadakis(mailTo:gpapadakis84@gmail.com)
+ *
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Inherited
+public @interface PostConfigure {
+
+    Class<?> classReference();
+
+    String method();
+
+    String[] arguments() default {""};
+
+}
