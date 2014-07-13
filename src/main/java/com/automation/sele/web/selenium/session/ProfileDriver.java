5de29a7f640a11248f4dc0e8aec93209aa1d479f
GiannisPapadakis <gpapadakis84@gmail.com>
2014-07-13 19:46:57 +0300

    profileDriver interface to be used as bean

---
+++ src/main/java/com/automation/sele/web/selenium/session/ProfileDriver.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.automation.sele.web.selenium.session;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Defines the type of Local Driver object
+ * @author Giannis Papadakis(mailTo:gpapadakis84@gmail.com)
+ *
+ */
+public interface ProfileDriver {
+	
+	/**Driver type*/
+	WebDriver profileDriver() throws Exception;
+	
+	
+}
