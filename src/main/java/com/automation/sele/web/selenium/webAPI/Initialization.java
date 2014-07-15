24261605d920f5f66934ad69535f3fca8ba2ecf2
GiannisPapadakis <gpapadakis84@gmail.com>
2014-07-15 20:57:59 +0300

    initialize javascript executor

--- src/main/java/com/automation/sele/web/selenium/webAPI/Initialization.java
+++ src/main/java/com/automation/sele/web/selenium/webAPI/Initialization.java
@@ -1,11 +1,15 @@
 /**
- * 
+ *
  */
 package com.automation.sele.web.selenium.webAPI;
 
 
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.activation.UnsupportedDataTypeException;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -27,13 +31,15 @@ public class Initialization {
 		Map<String, Object> controllers= new HashMap<>();
 
 		if(mode.compareToIgnoreCase("web")==0){
-			ActionsController<?> webcontroller = (ActionsController<?>) ApplicationContextProvider.getApplicationContext().getBean(WebDriverActionsController.class);
+			ActionsController<?> webcontroller = ApplicationContextProvider.getApplicationContext().getBean(WebDriverActionsController.class);
 
 			webcontroller = new WebDriverInitialize().initialize(webcontroller);
 
 			//put the objects for controllers to the Map
 			controllers.put(Constants.WEB_ACTIONS_CONTROLLER.get(), webcontroller);
 
+		} else {
+			throw new UnsupportedDataTypeException("The mode for testing is not set with!!!");
 		}
 		return controllers;
 	}
@@ -43,23 +49,26 @@ public class Initialization {
 	 * @author Giannis Papadakis(mailTo:gpapadakis84@gmail.com)
 	 *
 	 */
-	static class WebDriverInitialize 
+	static class WebDriverInitialize
 	{
-		
+
 		public ActionsController<?> initialize(ActionsController<?> control){
 
-			//Get the beans 
+			//Get the beans
 			WebDriverActionsController<?> wdActions = (WebDriverActionsController<?>) control;
 			WebDriver driver = null;
-			
-			//Create App Context for initializing driver 
+
+			//Create Application Context for initializing driver based on specified @Profile
 			AnnotationConfigApplicationContext app=new AnnotationConfigApplicationContext();
 			app.getEnvironment().setActiveProfiles(new String[]{Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest().getParameter(Constants.BROWSERTYPE.get())});
 			app.register(LocalDriverConfiguration.class);
 			app.refresh();
 			driver=(WebDriver) app.getBean("profileDriver");
+
+            //Set objects per session
 			wdActions.setDriver(driver);//set the driver object for this session
-            SessionContext.getSession().setDriverContext(app);//set the new application context for WebDriver
+			wdActions.setJsExec((JavascriptExecutor)driver);//sets tthe Javascript executor
+			SessionContext.getSession().setDriverContext(app);//set the new application context for WebDriver
 			return control;
 		}
 	}
