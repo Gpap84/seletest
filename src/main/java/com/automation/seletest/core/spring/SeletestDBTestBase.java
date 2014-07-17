81edddb69b7cc357712497fb998bea1e4cb58ded
GiannisPapadakis <gpapadakis84@gmail.com>
2014-07-17 20:24:35 +0300

    test base classes

---
+++ src/main/java/com/automation/seletest/core/spring/SeletestDBTestBase.java
@@ -0,0 +1,41 @@
+package com.automation.seletest.core.spring;
+
+import java.lang.reflect.Method;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.BeforeTransaction;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * This class is used as a base class for DB testing
+ * @author Giannis Papadakis(mailTo:gpapadakis84@gmail.com)
+ *
+ */
+@Transactional
+@TransactionConfiguration(defaultRollback = false)
+@ContextConfiguration({"classpath*:META-INF/spring/*-context.xml" })
+public abstract class SeletestDBTestBase extends AbstractTransactionalTestNGSpringContextTests {
+
+    @BeforeMethod(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
+    @Override
+    protected synchronized void springTestContextBeforeTestMethod(Method testMethod) throws Exception {
+        super.springTestContextBeforeTestMethod(testMethod);
+    }
+
+    @BeforeTransaction
+    public void beforeTransaction(){
+        //TODO
+    }
+
+    @AfterTransaction
+    public void afterTransaction(){
+        //TODO
+    }
+
+}
