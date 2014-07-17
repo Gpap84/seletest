36b2abbfcb27b9858a17282d2e160683a49a7bce
GiannisPapadakis <gpapadakis84@gmail.com>
2014-07-17 20:25:26 +0300

    log functions changing style attribute from tests

---
+++ src/main/java/com/automation/seletest/core/services/CoreProperties.java
@@ -0,0 +1,81 @@
+package com.automation.seletest.core.services;
+
+/**
+ * Enumeration for global constants
+ * @author Giannis Papadakis(mailTo:gpapadakis84@gmail.com)
+ *
+ */
+public enum CoreProperties {
+
+
+    /************************ Interfaces used **********************/
+    WEB_ACTIONS_CONTROLLER("webActionController"),
+
+    /**************************Parameters XML**********************/
+
+    /** The parameter that determines the browser type. */
+    BROWSERTYPE("browserType"),
+
+    /** The parameter that determines the performance measures. */
+    PERFORMANCE("performance"),
+
+    /** The parameter that determines the Grid host. */
+    GRID_HOST("gridHost"),
+
+    /** The parameter that determines the Grid port. */
+    GRID_PORT("gridPort"),
+
+
+    /*************************************CoreProperties used for test status-actions on UI*****************************************************/
+    PASS_COLOR("Chartreuse"),
+    FAIL_COLOR("FireBrick"),
+    ACTION_COLOR("BurlyWood"),
+
+    /*************************************CoreProperties used for initialization phase*****************************************************/
+
+    /** The parameter for true string*/
+    TRUE("true"),
+
+    /** The parameter for  false string*/
+    FALSE("false"),
+
+    /** The parameter that determines the test mode (Local environment or Grid) */
+    MODE("mode"),
+
+    //--------------------------------------------------    CoreProperties for locators --------------------->>>>>>>>>>>>>>>>>
+    /** The Constant XPATH. */
+    XPATH("xpath"),
+
+    /** The Constant CSS. */
+    CSS("css"),
+
+    /** The Constant NAME. */
+    NAME("name"),
+
+    /** The Constant LINK. */
+    LINK("link"),
+
+    /** The Constant ID. */
+    ID("id"),
+
+    /** The Constant TAGNAME. */
+    TAGNAME("tagname"),
+
+    /** The Constant JQUERY. */
+    JQUERY("jquery"),
+    ;
+
+    private String myLocator;
+
+    CoreProperties(String locator) {
+        myLocator = locator;
+    }
+
+    public String get() {
+        return myLocator;
+    }
+
+
+
+
+}
--- src/main/java/com/automation/seletest/core/services/Logging.java
+++ src/main/java/com/automation/seletest/core/services/Logging.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.automation.seletest.core.services;
 
@@ -19,48 +19,68 @@ import org.testng.Reporter;
 @Slf4j
 public class Logging {
 
-	//For reportNG HTML
-	@PostConstruct
-	public void init(){
-		System.setProperty("org.uncommons.reportng.escape-output", "false");
-		log.debug("org.uncommons.reportng.escape-output set to false for reporting");
-	}
 
-	/**
-	 * Info.
-	 *
-	 * @param message
-	 *            the message
-	 */
-	public void info(String message) {
-		log.info(message);
-		Reporter.log("<b><p class=\"testOutput\" style=\"color:MediumBlue; font-size:1em;\">" + message + "</p></b>");
-
-	}
-
-	/**
-	 * Warn.
-	 *
-	 * @param message
-	 *            the message
-	 */
-	public void warn(String message) {
-		log.warn(message);
-		Reporter.log("<b><p class=\"testOutput\" style=\"color:orange; font-size:1em;\">" + message + "</p></b>");
-	}
-
-	/**
-	 * Error.
-	 *
-	 * @param message
-	 *            the message
-	 */
-	public void error(String message) {
-		log.error(message);
-		Reporter.log("<b><p class=\"testOutput\" style=\"color:red; font-size:1em;\">" + message + "</p></b>");
-	}
+    @PostConstruct
+    public void init(){
+        System.setProperty("org.uncommons.reportng.escape-output", "false");
+        log.debug("'escape-output' set to false for reporting");
+    }
 
 
+    /**
+     * Info log to console and in HTML report
+     * @param message
+     * @param style
+     */
+    public void info(String message, String style) {
+        log.info(message);
+        Reporter.log("<b><p class=\"testOutput\" style="+style+">" + message + "</p></b>");
+
+    }
+
+    /**
+     * Default info
+     * @param message
+     */
+    public void info(String message) {
+        info(message, "\"color:MediumBlue; font-size:1em;\"");
+    }
+
+    /**
+     * Warn log to console and in HTML report
+     * @param message
+     * @param style
+     */
+    public void warn(String message, String style) {
+        log.warn(message);
+        Reporter.log("<b><p class=\"testOutput\" style="+style+">" + message + "</p></b>");
+    }
+
+    /**
+     *  Default warn
+     * @param message
+     */
+    public void warn(String message) {
+       warn(message, "\"color:orange; font-size:1em;\"");
+    }
+
+   /**
+     * Error log to console and in HTML report
+     * @param message
+     * @param style
+     */
+    public void error(String message, String style) {
+        log.error(message);
+        Reporter.log("<b><p class=\"testOutput\" style="+style+">" + message + "</p></b>");
+    }
+
+    /**
+     * Default error
+     * @param message
+     */
+    public void error(String message) {
+        error(message, "\"color:red; font-size:1em;\"");
+    }
 
 
 }
