fff5b4b58e26181946a4fd7f31f2bc09d01a859e
GiannisPapadakis <gpapadakis84@gmail.com>
2014-07-28 11:38:11 +0300

    aspect advices for Actions Builder

--- src/main/java/com/automation/seletest/core/aspectJ/ExceptionHandlingAspect.java
+++ src/main/java/com/automation/seletest/core/aspectJ/ExceptionHandlingAspect.java
@@ -1,3 +1,29 @@
+/*
+This file is part of the Seletest by Papadakis Giannis <gpapadakis84@gmail.com>.
+
+Copyright (c) 2014, Papadakis Giannis <gpapadakis84@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package com.automation.seletest.core.aspectJ;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -23,7 +49,6 @@ public class ExceptionHandlingAspect extends SuperAspect{
     @Autowired
     Logging log;
 
-
     /**
      * Around advice with custom annotation for retrying execution of JoinPoint
      * @param pjp
@@ -38,36 +63,13 @@ public class ExceptionHandlingAspect extends SuperAspect{
         for (int attemptCount = 1; attemptCount <= (1+retry.retryCount()); attemptCount++) {
             try {
                 returnValue = pjp.proceed();
-                log.info("Command: "+pjp.getSignature().getName()+" executed with arguments: "+arguments(pjp)+"!!!");
+                log.info("Command: "+pjp.getSignature().getName()+" executed successfully with arguments: "+arguments(pjp)+"!!!");
                 SessionControl.actionsController().changeStyle("backgroudColor", (String) (pjp).getArgs()[0], CoreProperties.ACTION_COLOR.get());
                 break;
             } catch (Exception ex) {
                 handleRetryException(pjp, ex, attemptCount, retry);
             }
         }
-        return returnValue;
-    }
-
-
-    /**
-     * Around advice for takeScreenshot functions
-     * @param pjp
-     * @param retry
-     * @return
-     * @throws Throwable
-     */
-    @Around("execution(* com.automation.seletest.core.selenium.webAPI.ActionsController.takeScreenShot*(..))")
-    public Object handleException(ProceedingJoinPoint pjp) throws Throwable
-    {
-        Object returnValue = null;
-        try {
-            returnValue = pjp.proceed();
-        } catch (Exception ex) {
-            log.error(String.format("%s: Failed with exception '%s'",
-                    pjp.getSignature().toString().substring(pjp.getSignature().toString().lastIndexOf(".")),
-                    ex.getMessage().split("Build")[0].trim()));
-        }
-
         return returnValue;
     }
 
@@ -83,7 +85,7 @@ public class ExceptionHandlingAspect extends SuperAspect{
             int attemptCount, RetryFailure retry) throws Throwable
     {
         if (ex instanceof TimeoutException) {
-            log.error("Element not found in Screen with exception: "+ex.getMessage().split("Build")[0].trim());
+            log.error("Element not found in screen with exception: "+ex.getMessage().split("Build")[0].trim());
             throw ex;
         } if (attemptCount == 1 + retry.retryCount()) {
             throw new RuntimeException(retry.message()+" for method: "+pjp.getKind(), ex);
